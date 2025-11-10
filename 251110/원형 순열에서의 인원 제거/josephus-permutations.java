cf9e706a5adc1ac42dad06424ab19f54dc3984e4
Taehyun Jung <jstm1207@gmail.com>
2025-11-10 12:36:30 +0900

    Title: 원형 순열에서의 인원 제거, Time: 284ms, Memory: 30MB, Status: Passed - Codetree

---
+++ 251110/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            que.add(i);
+        }
+
+        while(!que.isEmpty()) {
+            for (int i = 0; i < k-1; i++) {
+                que.add(que.poll());
+            }
+            System.out.print(que.poll() + " ");
+        }
+    }
+}
