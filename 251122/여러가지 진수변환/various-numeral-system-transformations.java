da2deb6c26ba6bd45185c4edfb5ab38a4fd045a9
Taehyun Jung <jstm1207@gmail.com>
2025-11-22 16:42:08 +0900

    Title: 여러가지 진수변환, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251122/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            sb.append(n % b);
+            n /= b;
+        }
+        sb.reverse();
+        System.out.print(sb.toString());
+    }
+}
