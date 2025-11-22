8f24afa80d305c53a83a23a78f8c564fccf18127
Taehyun Jung <jstm1207@gmail.com>
2025-11-23 08:16:03 +0900

    Title: 왔다 갔던 구역 2, Time: 141ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251123/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] array = new int[2000 + 1];
+        int start = 1001;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+            if (dir == 'R') {
+                for (int j = start; j < x + start; j++) {
+                    array[j]++;
+                }
+            } else {
+                x = -x;
+                for (int j = start; j > x + start; j--) {
+                    array[j]++;
+                }
+            }
+            start += x;
+        }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= 2) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
