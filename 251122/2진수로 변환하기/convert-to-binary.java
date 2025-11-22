d3330cf2112ae2fe3b443c201952d0c7e48bc1c4
Taehyun Jung <jstm1207@gmail.com>
2025-11-22 16:23:28 +0900

    Title: 2진수로 변환하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251122/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            sb.append(n % 2);
+            n /= 2;
+        }
+        sb.reverse();
+        System.out.print(sb.toString());
+    }
+}
